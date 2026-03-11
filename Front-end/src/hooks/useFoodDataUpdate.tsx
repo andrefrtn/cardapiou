925ea53a5fe0d98846d43c66c162d60b2f552fc3
André Fortini <124889918+andrefrtn@users.noreply.github.com>
2026-03-11 20:44:54 -0300

    Update API URL for food data update

--- Front-end/src/hooks/useFoodDataUpdate.tsx
+++ Front-end/src/hooks/useFoodDataUpdate.tsx
@@ -2,14 +2,15 @@ import { useMutation, useQueryClient } from "@tanstack/react-query"
 import axios from "axios"
 import type { FoodData } from "../interface/FoodData"
 
+const API_URL = "https://cardapiou.onrender.com"
+
 const updateFood = async (food: FoodData) => {
-  const response = await axios.put(`http://localhost:8080/food/${food.id}`, food)
+  const response = await axios.put(`${API_URL}/food/${food.id}`, food)
   return response.data
 }
 
 export function useFoodDataUpdate() {
   const queryClient = useQueryClient()
-
   return useMutation({
     mutationFn: updateFood,
     onSuccess: () => {
